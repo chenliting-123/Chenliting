f1d7aa0a0151878d84045f7cfd279e0fe37892ee
Alisa <alisa@Alisas-MacBook-Pro.local>
2021-02-22 10:58:44 +0800

    跟教程修改

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,18 +18,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'first-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('first-app app is running!');
+    // 启动angular的自动变更监测机制，自动对v层的数据进行渲染
+    fixture.autoDetectChanges();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+
+  // 定义教师数组
+  teachers = [{
+    id: 1,
+    name: '张三',
+    username: 'zhangsan',
+    email: 'zhangsan@yunzhiclub.com',
+    sex: true,
+  }, {
+    id: 2,
+    name: '李四',
+    username: 'lisi',
+    email: 'lisi@yunzhiclub.com',
+    sex: false,
+  }];
 }
